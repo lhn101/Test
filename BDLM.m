c9b3ecb9ff222787f8fb28001059ee5e4b082544
HV <luong-ha.nguyen@polymtl.ca>
2017-08-27 09:58:43 -0400

    final version

--- BDLM.m
+++ BDLM.m
@@ -192,7 +192,6 @@ elseif user_inputs.inp_1==3
     estim=state_estimation(data,model,option);
     plot_estimations(estim,data,model,option) 
     %test
-    %i dont want this please remove it. I cant not remove it
 elseif user_inputs.inp_1==11
     disp(' ')
     disp('-------------------------------------------------------------------------------')
